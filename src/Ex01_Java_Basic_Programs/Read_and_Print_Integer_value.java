dd9853e9f508b2ee2eba05c68e09dd5cba08354e
DUSAWANT3 <durgesh3230@gmail.com>
2025-01-15 11:59:31 +0530

    day1

---
+++ src/Ex01_Java_Basic_Programs/Read_and_Print_Integer_value.java
@@ -0,0 +1,27 @@
+package Ex01_Java_Basic_Programs;
+
+import java.util.Scanner;
+
+public class Read_and_Print_Integer_value {
+    public static void main(String[] args) {
+
+        // Declare the variables
+        int num;
+
+        // Input the integer
+        System.out.println("Enter the integer: ");
+
+        // Create Scanner object
+        Scanner s = new Scanner(System.in);
+
+        // Read the next integer from the screen
+        num = s.nextInt();
+
+        // Display the integer
+        System.out.println("Entered integer is: " + num);
+
+        // Close the Scanner object
+        s.close(); // Important to prevent resource leaks
+    }
+}
+
