1db029c74bc624cf9f5f1165253c391409807291
DUSAWANT3 <durgesh3230@gmail.com>
2025-05-05 10:07:36 +0530

    0505

--- src/Ex03_Strings/Ex01_Check_String_is_a_Palindrome.java
+++ src/Ex03_Strings/Ex01_Check_String_is_a_Palindrome.java
@@ -2,16 +2,17 @@ package Ex03_Strings;
 
 public class Ex01_Check_String_is_a_Palindrome {
     //Check if a String is a Palindrome
-    //A palindrome is a word, phrase, or number that reads the same backward as forward.
+    // is a word, phrase, or number that reads the same backward as forward.
     //Examples:
-    //✅ madam, racecar, 121
+    //✅ madam, racecar, 121, nitin
     //❌ hello, java, world
     public static void main(String[] args) {
         //Method 01 -Using Two-Pointer Technique
         String str = "madam";
 
         boolean isPalindrome = true;
-        int start = 0, end = str.length() - 1;
+        int start = 0;
+        int end = str.length() - 1;
 
         while (start < end) {
             if (str.charAt(start) != str.charAt(end)) {
@@ -26,6 +27,5 @@ public class Ex01_Check_String_is_a_Palindrome {
         } else {
             System.out.println(str + " is not a palindrome.");
         }
-
     }
 }
