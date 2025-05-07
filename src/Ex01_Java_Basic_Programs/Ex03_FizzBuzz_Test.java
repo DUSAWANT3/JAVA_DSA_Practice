5e9dde49fe68c227ab55408edd9afd18897bd98e
DUSAWANT3 <durgesh3230@gmail.com>
2025-05-07 10:03:28 +0530

    0507

--- src/Ex01_Java_Basic_Programs/Ex03_FizzBuzz_Test.java
+++ src/Ex01_Java_Basic_Programs/Ex03_FizzBuzz_Test.java
@@ -6,13 +6,15 @@ public class Ex03_FizzBuzz_Test {
         //However, for Multiples of 3, print "Fizz" instead of the number,
         // and for multiples of 5, print "Buzz." For numbers that are multiples of both 3 and 5, print "FizzBuzz."
 
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) { //Divide i by 3, and return the remainder.
                 System.out.println(i + " = FizzBuzz");
             } else if (i % 3 == 0) {
                 System.out.println(i + " = Fizz");
             } else if (i % 5 == 0) {
                 System.out.println(i + " = Buzz");
+            } else {
+               // System.out.println(i);
             }
         }
     }
