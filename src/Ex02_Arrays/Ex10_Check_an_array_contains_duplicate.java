25910168fdc46bb3730a209174226f43d52d5e5a
DUSAWANT3 <durgesh3230@gmail.com>
2025-05-14 09:53:31 +0530

    0514

--- src/Ex02_Arrays/Ex10_Check_an_array_contains_duplicate.java
+++ src/Ex02_Arrays/Ex10_Check_an_array_contains_duplicate.java
@@ -10,20 +10,20 @@ public class Ex10_Check_an_array_contains_duplicate {
     public static void main(String[] args) {
         int[] arr = {3, 5, 1, 7, 9, 3}; // Duplicate: 3
         Boolean duplicate = false;
-        for(int i =0; i<arr.length; i++){
-            for(int j =i+1; j<arr.length; j++ ){
-                if(arr[i]==arr[j]){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
                     duplicate = true;
                 }
             }
         }
-        if(duplicate==true){
+        if (duplicate == true) {
             System.out.println("Array is Duplicate Elements");
-        }else {
+        } else {
             System.out.println("Array is not Duplicate Elements");
         }
 
-  //Using Java 8
+        //Using Java 8
         Set<Integer> seen = new HashSet<>();
 
         boolean hasDuplicate = IntStream.of(arr)
