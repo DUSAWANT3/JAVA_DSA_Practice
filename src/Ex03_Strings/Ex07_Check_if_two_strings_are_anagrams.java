79a95bd3e70f83ea63b593f294dc1348d3b737a3
DUSAWANT3 <durgesh3230@gmail.com>
2025-05-16 22:22:06 +0530

    0516

--- src/Ex03_Strings/Ex07_Check_if_two_strings_are_anagrams.java
+++ src/Ex03_Strings/Ex07_Check_if_two_strings_are_anagrams.java
@@ -12,46 +12,28 @@ public class Ex07_Check_if_two_strings_are_anagrams {
         //With the same frequency,
         //But possibly in a different order.
         //"listen", "silent" /"hello", "holle"
-//Using HashMap
-        String str1 = "listen";
-        String str2 = "silent";
 
-        if (areAnagrams(str1, str2)) {
-            System.out.println("✅ Strings are anagrams");
+        String s1 = "listen"; //Tea & Eat
+        String s2 = "silent";
+
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+
+        if (s1.length() == s2.length()) {
+            char[] Array1 = s1.toCharArray();
+            char[] Array2 = s2.toCharArray();
+
+            Arrays.sort(Array1);
+            Arrays.sort(Array2);
+            boolean result = Arrays.equals(Array1, Array2);
+            if (result) {
+                System.out.println("Is  anagram");
+            } else {
+                System.out.println("Is not anagram");
+            }
         } else {
-            System.out.println("❌ Strings are not anagrams");
+            System.out.println("Is not anagram");
         }
-    }
-    public static boolean areAnagrams(String s1, String s2) {
-  // Step 1: Length check
-        if (s1.length() != s2.length()) {
-            return false;
-        }
- // Step 2: Frequency map
-        Map<Character, Integer> freqMap = new HashMap<>();
-        // Count frequency in s1
-        for (char c : s1.toCharArray()) {
-            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
-        }
-  // Subtract frequency using s2
-        for (char c : s2.toCharArray()) {
-            if (!freqMap.containsKey(c)) return false;
 
-            freqMap.put(c, freqMap.get(c) - 1);
-            if (freqMap.get(c) < 0) return false;
-        }
-        return true; // All frequencies matched
-    }
-    //----------------------------------------------------------
-    public static boolean isAnagramBySorting(String s1, String s2) {
-        if (s1.length() != s2.length()) return false;
-
-        char[] arr1 = s1.toCharArray();
-        char[] arr2 = s2.toCharArray();
-
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-
-        return Arrays.equals(arr1, arr2);
     }
 }
--- src/Ex99_Practice/Ex09_1605.java
+++ src/Ex99_Practice/Ex09_1605.java
@@ -1,5 +1,8 @@
 package Ex99_Practice;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 public class Ex09_1605 {
     public static void main(String[] args) {
         //Ex04 reverse a string
@@ -17,5 +20,33 @@ public class Ex09_1605 {
         } else {
             System.out.println("Strings are not equal");
         }
+
+        System.out.println("44444444444444444444444444444444");
+
+   //Tea & Eat
+        String s1= "Tea";
+        String s2 = "Eat";
+        //lower case
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+        //length
+        if(s1.length() == s2.length()){
+            char[] Array1 = s1.toCharArray();
+            char[]  Array2 = s2.toCharArray();
+
+            Arrays.sort(Array1);
+            Arrays.sort(Array2);
+             boolean isEqual = Arrays.equals(Array1,Array2);
+            if(isEqual){
+                System.out.println("is Equal");
+            }else{
+                System.out.println("is not Equal");
+            }
+        }else {
+            System.out.println("is not Equal");
+        }
+
+
+
     }
 }
