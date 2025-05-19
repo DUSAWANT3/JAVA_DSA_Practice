9016d4d0833948466bfc1abbd7250fce76beb235
DUSAWANT3 <durgesh3230@gmail.com>
2025-05-19 19:18:23 +0530

    0519

---
+++ src/Ex03_Strings/Ex12_Check_String_Valid_Shuffle_of_Two_Strings.java
@@ -0,0 +1,13 @@
+package Ex03_Strings;
+
+public class Ex12_Check_String_Valid_Shuffle_of_Two_Strings {
+    public static void main(String[] args) {
+        //Ex12_Check if a String is a Valid Shuffle of Two Strings
+       //Given three strings: str1, str2, and result, check if result is a valid shuffle of str1 and str2.
+        //A valid shuffle means:
+        //result contains all characters from str1 and str2, and
+        //the order of characters in str1 and str2 is preserved in result.
+
+
+    }
+}
