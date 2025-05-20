dc2a58b5c3c027a3514c96ac3e5aa662a22f96c6
DUSAWANT3 <durgesh3230@gmail.com>
2025-05-20 08:39:36 +0530

    0520

---
+++ src/Ex06_Linked_List/Ex01_Linked_List.java
@@ -0,0 +1,25 @@
+package Ex06_Linked_List;
+
+public class Ex01_Linked_List {
+    public static void main(String[] args) {
+        //✅Linked List
+        //✅ 1. What is a Linked List?
+     //A Linked List is a linear data structure where elements (nodes) are connected using pointers.
+        //Each node contains:
+        //•	Data (value)
+        //•	Reference (pointer) to the next node
+    //Unlike arrays:
+        //•	Memory is not contiguous
+        //•	Size is dynamic
+        //•	Insertion/Deletion is efficient (no shifting)
+
+//2. ✅ Types of Linked Lists
+//Type	Description
+//Singly Linked List	Each node has data + a pointer to the next node only
+//Doubly Linked List	Each node has data + a pointer to next & previous nodes
+//Circular Linked List	The last node points back to the head (can be singly or doubly circular)
+
+
+
+    }
+}
