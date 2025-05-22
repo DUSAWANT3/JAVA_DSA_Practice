aae3667d310e87809bc53f0a5bb60d8cd6660cd3
DUSAWANT3 <durgesh3230@gmail.com>
2025-05-22 13:46:48 +0530

    0522

--- src/Ex03_Strings/Ex00_Strings.java
+++ src/Ex03_Strings/Ex00_Strings.java
@@ -27,5 +27,29 @@ public class Ex00_Strings {// Java Program to demonstrate String
         String result = (num % 2 == 0) ? "Even" : "Odd";
         System.out.println(result); //odd
 
+        System.out.println("*************************************************");
+//✅immutable Why are strings immutable in Java?
+ //A String in Java is a sequence of characters. It's an immutable object of the String class, meaning it can't be changed once created.A String in Java is a sequence of characters.
+// It's an immutable object of the String class, meaning it can't be changed once created.
+
+     String str1 = "Durgesh";//Java places "Durgesh" in the String pool.
+     String str2 = "Durgesh"; //Java sees that "Durgesh" already exists in the pool, so it reuses the reference
+     String str3 = new String("Durgesh"); //This explicitly creates a new String object in the heap, even though the content is the same.
+
+        System.out.println(str1==str2); //true because it's pointing to same object/reference String pool.
+        System.out.println(str1.equals(str2));//true contain the same value
+
+        System.out.println(str1 == str3);//false because it's pointing to different object
+        System.out.println(str1.equals(str3));//true
+
+        String s = "Java";
+        s.concat(" Rocks!");
+        System.out.println(s); // "Java" — original string unchanged
+
+        String newStr = s.concat(" Rocks!");
+        System.out.println(newStr); // "Java Rocks!" — new string created
+
+        //Strings in Java are immutable to ensure security, thread safety, efficient memory usage via the String pool,
+        // hashcode caching, and overall simplicity in application design.
     }
 }
