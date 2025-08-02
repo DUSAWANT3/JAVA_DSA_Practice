9cb7cc747c7c8daa62e608b6b15cbea7b2c388b2
DUSAWANT3 <durgesh3230@gmail.com>
2025-08-02 19:39:59 +0530

    2407

--- src/EX991_Practice/Ex01.java
+++ src/EX991_Practice/Ex01.java
@@ -1,8 +1,12 @@
 package EX991_Practice;
 
 public class Ex01 {
+    String src = "//input[@name=\"userName\"]";
 
+    long count = src.chars()                      // Convert to IntStream
+            .filter(Character::isLetter)   // Keep only alphabetic characters
+            .count();                      // Count them
 
-
-
+        System.out.println("Total alphabetic characters: " + count);
 }
+
