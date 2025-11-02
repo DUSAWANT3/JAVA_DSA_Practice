a177fa2fbbccd609652aa4ce42293ff098241711
DUSAWANT3 <durgesh3230@gmail.com>
2025-11-02 11:23:26 +0530

    1102

---
+++ src/Ex07_StringLetters_manipulation_fundamentals/Ex02_Ex01_Convert_lowercase_to_uppercase_and_vice_versa.java
@@ -0,0 +1,12 @@
+package Ex07_StringLetters_manipulation_fundamentals;
+
+public class Ex02_Ex01_Convert_lowercase_to_uppercase_and_vice_versa {
+    public static void main(String[] args) {
+
+        //Input: "HeLLo WoRLd"  -- > Output: "hEllO wOrlD"
+
+
+
+
+    }
+}
