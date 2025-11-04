19c39daf3e51962bd21449be7cf70a3fa4aaface
DUSAWANT3 <durgesh3230@gmail.com>
2025-11-04 20:37:40 +0530

    1102

--- src/Ex07_StringLetters_manipulation_fundamentals/EX99.java
+++ src/Ex07_StringLetters_manipulation_fundamentals/EX99.java
@@ -1,8 +1,23 @@
 package Ex07_StringLetters_manipulation_fundamentals;
 
+import java.sql.SQLOutput;
+
 public class EX99 {
     public static void main(String[] args) {
+        String input = "Durgesh Sawant";
+        String output = "";
 
+        for(int i=0; i< input.length(); i++){
+            char ch = input.charAt(i);
+            if(Character.isUpperCase(ch)){
+                output+= Character.toLowerCase(ch);
+            } else if (Character.isLowerCase(ch)) {
+                output += Character.toUpperCase(ch);
+            }else {
+                output += ch;
+            }
+        }
 
+        System.out.println(output);
     }
 }
