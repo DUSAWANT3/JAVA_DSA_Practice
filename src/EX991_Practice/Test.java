0261ff6524d19f7974b72b4af981292776b9b446
DUSAWANT3 <durgesh3230@gmail.com>
2025-11-24 16:32:18 +0530

    2411

--- src/EX991_Practice/Test.java
+++ src/EX991_Practice/Test.java
@@ -6,5 +6,20 @@ public class Test {
 
 
 
+        int[] arr = {10,20,30,40,50,100};
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i =1; i< arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println("min  " + min);
+        System.out.println("Max   " + max);
     }
 }
